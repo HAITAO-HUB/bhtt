f10af98743c10c61b746f6bb47a62e52cd0ba3e1
HAITAO-HUB <15291118967@163.com>
2019-09-22 14:24:12 +0800

    new

--- 单链表/main.c
+++ 单链表/main.c
@@ -1,13 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+/*Node*LastKNode(Plist plist,int n)//寻找单链表中倒数第n个结点
+{
+	Node*q = plist;
+	Node*p = plist;
+	for (int i = 0; i < n-1;i++)
+	{
+		p = p->next;
+		if (p->next == NULL)
+		{
+			printf("没有此结点");
+			break;
+		}
+	}
+	while (p!=NULL)
+	{
+		p = p->next;
+		q = q->next;
+	}
+	return q;
+}
+*/
 #include"list.h"
 int main()
 {
-	Node Head;
-	Intlist(&Head);
-	InsertHead(&Head, 10);
-	InsertHead(&Head, 20);
-	InsertHead(&Head, 30);
-	InsertHead(&Head, 40);
-	Show(&Head);
-	return 0;
+	Node head;
+	intlist(&head);
+	InsertHead(&head,10);
+	InsertHead(&head,20);
+	InsertHead(&head,30);
+	InsertHead(&head,40);
+	Show(&head);
 }
